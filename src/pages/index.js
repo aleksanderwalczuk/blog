b8193c0bd561f366d6b0a0656029d1a4e75235d6
rednaxela700 <ohleg@windowslive.com>
2019-12-28 22:54:56 +0100

    added markup for post paragraph, centered img

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,25 @@ export default () => (
           <h3 className="heading post-heading">Dupa 1</h3>
           <span>{time}</span>
         </div>
-        <img src="https://picsum.photos/700/450" alt="" className="post-img" />
+        <figure className="post-img-container">
+          <img
+            src="https://picsum.photos/700/450"
+            alt=""
+            className="post-img"
+          />
+        </figure>
+        <p className="post-paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+          aliquam.
+        </p>
+        <p className="post-paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+          aliquam.
+        </p>
+        <p className="post-paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+          aliquam.
+        </p>
       </section>
     </article>
   </Layout>
