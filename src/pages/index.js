9ca389d20cd21baf9250fca864665a544e172d63
rednaxela700 <ohleg@windowslive.com>
2019-12-28 22:23:33 +0100

    added example post markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
 
+const time = new Date().toLocaleDateString()
+
 export default () => (
   <Layout>
     <h1 className="heading">Witaj świecie,</h1>
     <p>W tym miejscu znajdą się moje zapiski, początkującego frontend deva!</p>
     <article className="article">
-      <section>
-        <h3>Dupa 1</h3>
+      <section className="post">
+        <div className="post-heading-container">
+          <h3 className="heading post-heading">Dupa 1</h3>
+          <span>{time}</span>
+        </div>
+        <img src="https://picsum.photos/700/450" alt="" className="post-img" />
       </section>
     </article>
   </Layout>
