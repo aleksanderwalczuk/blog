aba2724d8c98b20c0ae5b8cac24afdced24e6ca9
rednaxela700 <ohleg@windowslive.com>
2019-12-28 21:39:52 +0100

    added example post markup to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,12 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1 className='heading'>Witaj świecie,</h1>
-    <p>
-      W tym miejscu znajdą się moje zapiski, początkującego frontend deva!
-    </p>
+    <h1 className="heading">Witaj świecie,</h1>
+    <p>W tym miejscu znajdą się moje zapiski, początkującego frontend deva!</p>
+    <article className="article">
+      <section>
+        <h3>Dupa 1</h3>
+      </section>
+    </article>
   </Layout>
 )
