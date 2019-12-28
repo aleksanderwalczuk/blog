85091009a11925020e37993b0a22c47421447a00
rednaxela700 <ohleg@windowslive.com>
2019-12-28 23:21:28 +0100

    added props to Post in post.js, saving picsum path and time in variables

--- src/components/post.js
+++ src/components/post.js
@@ -1,15 +1,31 @@
 import React from "react"
 
-const Post = props => (
-  <div className={''}>
-    <img src={props.avatar} alt="" />
-    <div className={''}>
-      <h2 className={'post-title'}>{props.postHeading}</h2>
-      <p className={'post-paragraph'}>{props.excerpt}</p>
-    </div>
-  </div>
-)
+const time = new Date().toLocaleDateString()
+const imgSrc = "https://picsum.photos/700/450"
 
-export default ({ children }) => (
-  <div className={''}>{children}</div>
+export const Post = props => (
+  <section className="post">
+    <div className="post-heading-container">
+      <h3 className="heading post-heading">{props.postHeading}</h3>
+      <span>{time}</span>
+    </div>
+    <figure className="post-img-container">
+      <img src={props.imgSrc} alt="blog post img" className="post-img" />
+    </figure>
+    <div>
+      {children || null}
+      <p className="post-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+        aliquam.
+      </p>
+      <p className="post-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+        aliquam.
+      </p>
+      <p className="post-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
+        aliquam.
+      </p>
+    </div>
+  </section>
 )
