0c3295d9c784b53b27da2db0cfa91c58e7946f4b
rednaxela700 <ohleg@windowslive.com>
2019-12-28 17:47:08 +0100

    added layout component to pages

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./about-me.module.css"
 import Container from "../components/container"
+import Layout from "../components/layout"
 
 const User = props => (
   <div className={styles.user}>
@@ -12,19 +13,23 @@ const User = props => (
   </div>
 )
 export default () => (
-  <Container>
-    <h1>About me</h1>
-    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
-    
-    <User
-      username="John Doe"
-      avatar="https://picsum.photos/seed/picsum/100/200"
-      excerpt="Hi! My name is John Doe."
-    />
-    <User
-      username="Jane Doe"
-      avatar="https://picsum.photos/100/200?grayscale"
-      excerpt="Hi! My name is Jane Doe. "
-    />
-  </Container>
+  <Layout>
+    <Container>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
+
+      <User
+        username="John Doe"
+        avatar="https://picsum.photos/seed/picsum/100/200"
+        excerpt="Hi! My name is John Doe."
+      />
+      <User
+        username="Jane Doe"
+        avatar="https://picsum.photos/100/200?grayscale"
+        excerpt="Hi! My name is Jane Doe. "
+      />
+    </Container>
+  </Layout>
 )
