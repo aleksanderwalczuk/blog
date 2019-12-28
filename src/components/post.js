2676f50be4123239093cc1c3ed34530f5fb70925
rednaxela700 <ohleg@windowslive.com>
2019-12-29 00:23:11 +0100

    fixed children in post.js, deleted unused var

--- src/components/post.js
+++ src/components/post.js
@@ -1,9 +1,8 @@
 import React from "react"
 
 const time = new Date().toLocaleDateString()
-const imgSrc = "https://picsum.photos/700/450"
 
-export const Post = props => (
+export const Post = (props) => (
   <section className="post">
     <div className="post-heading-container">
       <h3 className="heading post-heading">{props.postHeading}</h3>
@@ -13,19 +12,7 @@ export const Post = props => (
       <img src={props.imgSrc} alt="blog post img" className="post-img" />
     </figure>
     <div>
-      {children || null}
-      <p className="post-paragraph">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
-        aliquam.
-      </p>
-      <p className="post-paragraph">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
-        aliquam.
-      </p>
-      <p className="post-paragraph">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis,
-        aliquam.
-      </p>
+      {props.children}
     </div>
   </section>
 )
