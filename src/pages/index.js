df1d062003e45b5b0817cbf2ebd64dfac931bd75
rednaxela700 <ohleg@windowslive.com>
2019-12-29 01:15:53 +0100

    put extenstions into separate arrays, added ListItem component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,49 +6,68 @@ import { Link } from "gatsby"
 const postImgs = [
   "https://pixabay.com/get/57e0d2454f51aa14f1dc8460825668204022dfe05456744e772c78d2/code-1076536_640.jpg",
 ]
-const extenstionsNames = [
-  // { 0: "", 1: "" },
+const look = ["Material Icon Theme", "One Dark Pro", "VSCode Great Icons"]
+
+const codecompletion = [
   "Auto Rename Tag",
-  "Beautify",
-  "Bracket Pair Colorizer",
   "Color Picker",
-  "CSS Peek",
-  "Debugger for Chrome",
-  "docs-markdown",
-  "ES7 React/Redux/GraphQL/ReactNative snippets",
-  "ES Lint",
-  "gi",
-  "Git Blame",
-  "GitLens",
-  "Ignore files",
-  "Image preview",
-  "Indented Block Highlighting",
   "Intellisense for CSS",
-  "JavaScript ES6 code snippets",
-  "JetBrains IDE Keymap",
-  "JS JSX Snippets",
-  "Live Sass Compiler",
-  "Live Server",
-  "Live Share",
-  "Markdown Preview Enchanced",
-  "markdownlint",
-  "Material Icon Theme",
-  "One Dark Pro",
-  "Paste and Indent",
   "Path Intellisense",
-  "Preetier - Code formatter",
-  "Quokka.js",
+  "docs-markdown",
+  "JavaScript ES6 code snippets",
+  "ES7 React/Redux/GraphQL/ReactNative snippets",
+  "Visual Studio Intellisense",
+  "JS JSX Snippets",
+]
+const git = ["gi", "Git Blame", "Git Graph", "GitLens", "Ignore files"]
+const sass = [
+  "Live Sass Compiler",
   "Sass",
   "Sass Yah",
+  "CSS Peek",
   "SCSS Intellisense",
-  "Trailing Spaces",
-  "Visual Studio Intellisense",
-  "VSCode Great Icons",
 ]
+
+const formatters = [
+  "Beautify",
+  "Preetier - Code formatter",
+  "Trailing Spaces",
+  "Paste and Indent",
+]
+const preview = [
+  "Live Server",
+  "Markdown Preview Enchanced",
+  "Quokka.js",
+  "Image preview",
+]
+const extenstionsNames = [
+  "Bracket Pair Colorizer",
+  "Debugger for Chrome",
+  "ES Lint",
+  "Indented Block Highlighting",
+  "JetBrains IDE Keymap",
+  "Live Share",
+  "markdownlint",
+]
+
+const ListItem = props => (
+  <li>
+    {props.category}
+    <ul>
+      {props.arr.map(item => (
+        <li>{item}</li>
+      ))}
+    </ul>
+  </li>
+)
 export default () => (
   <Layout>
-    <h1 className="heading">Witaj świecie,</h1>
-    <p>W tym miejscu znajdą się moje zapiski, początkującego frontend deva!</p>
+    <div className="aside">
+      <h1 className="heading">Witaj świecie</h1>
+      <p>
+        W tym miejscu znajdą się moje zapiski, początkującego frontend deva!
+      </p>
+    </div>
     <article className="article">
       <Post postHeading="Dodatki do VS Code, mój config" imgSrc={postImgs[0]}>
         <p className="post-paragraph">
@@ -58,11 +77,17 @@ export default () => (
           sekcję&nbsp;
           <Link to="about-me">O mnie</Link>. VSC to mój edytor nr. 1, chociaż
           wolę korzystać z IDE, a dokładniej Webstorma. Postaram się je
-          podzielić na kategorie i posortować. Opiszę też te, które mogą być warte uwagi, a ich nazwa nie mówi sama za siebie. Nie przedłużając moje dodatki to:
+          podzielić na kategorie i posortować. Opiszę też te, które mogą być
+          warte uwagi, a ich nazwa nie mówi sama za siebie. Oglądajcie,
+          testujcie:
           <ol>
-            {extenstionsNames.map(item => (
-              <li>{item}</li>
-            ))}
+            <ListItem category="Wygląd: " arr={look} />
+            <ListItem category="Formattery: " arr={formatters} />
+            <ListItem category="Code Completion: " arr={codecompletion} />
+            <ListItem category="Sass/CSS: " arr={sass} />
+            <ListItem category="Live preview: " arr={preview} />
+            <ListItem category="Git: " arr={git} />
+            <ListItem category="Pozostałe: " arr={extenstionsNames} />
           </ol>
         </p>
       </Post>
