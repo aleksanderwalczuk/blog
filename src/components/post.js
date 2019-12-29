35bb6d2eb0509eccddfd0fa01eb90c6c9f05b28b
rednaxela700 <ohleg@windowslive.com>
2019-12-29 13:12:18 +0100

    deleted time const, passing via props

--- src/components/post.js
+++ src/components/post.js
@@ -1,12 +1,11 @@
 import React from "react"
 
-const time = new Date(2019,12,28).toLocaleDateString('pl_PL')
 
 export const Post = (props) => (
   <section className="post">
     <div className="post-heading-container">
       <h3 className="heading post-heading">{props.postHeading}</h3>
-      <span>{time}</span>
+      <span>{props.time}</span>
     </div>
     <figure className="post-img-container">
       <img src={props.imgSrc} alt="blog post img" className="post-img" />
