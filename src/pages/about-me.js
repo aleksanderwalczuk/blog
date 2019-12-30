b33ef10ab6855d2b6f1b6524c7f3edf54b578126
rednaxela700 <ohleg@windowslive.com>
2019-12-30 12:37:44 +0100

    changed text in about me

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -26,11 +26,10 @@ export default () => (
         <div className="about-author">
           <img src={aboutImg} alt="author img" className="about-author-img" />
           <p className="post-paragraph about-author-paragraph">
-            Cześć nazywam się Aleksander. Jestem programistą, tenisistą i
-            narciarzem. Piszę tutaj o tym czego się uczę jako początkujący
-            frontendowiec i jak wygląda mój sposób pracy. Chętnie współpracuję i
-            uczę się nowych technologii. Masz pomysł na apkę? Szukasz osób do
-            pracy? Zapraszam, odwiedź zakładkę{" "}
+            Cześć nazywam się Aleksander. Piszę tutaj o tym czego się uczę jako
+            początkujący frontendowiec i jak wygląda mój sposób pracy. Chętnie
+            współpracuję i uczę się nowych technologii. Masz pomysł na apkę?
+            Szukasz osób do pracy? Zapraszam, odwiedź zakładkę{" "}
             <Link to="./contact" className="post-link">
               Kontakt.
             </Link>
